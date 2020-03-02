14b8427ad47212a032a897cf8f4f96573f6b7fe1
Lutfur Rahman Sarker <eng.lrsarker@gmail.com>
2020-03-02 23:55:41 +0600

    Sorr for this project

--- product.js
+++ product.js
@@ -1,17 +1,3 @@
- //quantity button event handler plus
- const quantityBtnPlus = document.getElementById("quantity-plus");
-    quantityBtnPlus.addEventListener("click", function(){
-
-    const productQuantity = document.getElementById("product-quantity").value ++;
- 
- })
- //quantity button event handler minus
- const quantityBtnMinus = document.getElementById("quantity-minus");
- quantityBtnMinus.addEventListener("click", function(){
-
-    const productQuantity = document.getElementById("product-quantity").value --;
-
- })
 //Sorry!
 
     
